bad00d64dc2ee55aa761a6a5144c1e7b7a9f2bf2
rachel275 <80170552+rachel275@users.noreply.github.com>
2024-12-17 11:28:43 +0000

    Update pthread_neon_strassen.c to take in square matrices of any size

--- pthread_neon_strassen.c
+++ pthread_neon_strassen.c
@@ -3,104 +3,112 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define MIN_MATRIX_SIZE 2  // The smallest submatrix to compute directly
+
 typedef struct {
     float64x2_t* C;
     float64x2_t* A;
     float64x2_t* B;
+    int size;
 } ThreadData;
 
-void matrix_addition_2x2(float64x2_t* C, float64x2_t* A, float64x2_t* B) {
-    for (int i = 0; i < 2; ++i) {
+void matrix_addition(float64x2_t* C, float64x2_t* A, float64x2_t* B, int size) {
+    for (int i = 0; i < size * size / 2; ++i) {
         C[i] = vaddq_f64(A[i], B[i]);
     }
 }
 
-void matrix_subtraction_2x2(float64x2_t* C, float64x2_t* A, float64x2_t* B) {
-    for (int i = 0; i < 2; ++i) {
+void matrix_subtraction(float64x2_t* C, float64x2_t* A, float64x2_t* B, int size) {
+    for (int i = 0; i < size * size / 2; ++i) {
         C[i] = vsubq_f64(A[i], B[i]);
     }
 }
 
-void matrix_multiplication_2x2(float64x2_t* C, float64x2_t* A, float64x2_t* B) {
-    for (int i = 0; i < 2; ++i) {
-        float64x2_t a0 = vdupq_n_f64(vgetq_lane_f64(A[i], 0));
-        float64x2_t a1 = vdupq_n_f64(vgetq_lane_f64(A[i], 1));
-        C[i] = vmulq_f64(a0, B[0]);
-        C[i] = vfmaq_f64(C[i], a1, B[1]);
+void matrix_multiplication(float64x2_t* C, float64x2_t* A, float64x2_t* B, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size / 2; ++j) {
+            C[i * size / 2 + j] = vmulq_f64(A[i * size / 2], B[j]);
+            for (int k = 1; k < size; ++k) {
+                C[i * size / 2 + j] = vfmaq_f64(C[i * size / 2 + j], A[i * size / 2 + k], B[k * size / 2 + j]);
+            }
+        }
     }
 }
 
-void* compute_strassen_2x2(void* arg) {
+void* compute_strassen(void* arg) {
     ThreadData* data = (ThreadData*)arg;
-    matrix_multiplication_2x2(data->C, data->A, data->B);
+    matrix_multiplication(data->C, data->A, data->B, data->size);
     return NULL;
 }
 
-void strassen_2x2(float64x2_t* C, float64x2_t* A, float64x2_t* B) {
-    float64x2_t M1[2], M2[2], M3[2], M4[2], M5[2], M6[2], M7[2];
-    float64x2_t tempA[2], tempB[2];
+void strassen(float64x2_t* C, float64x2_t* A, float64x2_t* B, int size) {
+    if (size <= MIN_MATRIX_SIZE) {
+        matrix_multiplication(C, A, B, size);
+        return;
+    }
+
+    int newSize = size / 2;
+    int subMatrixSize = newSize * newSize / 2;
+    float64x2_t *M1 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M2 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M3 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M4 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M5 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M6 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *M7 = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *tempA = malloc(subMatrixSize * sizeof(float64x2_t));
+    float64x2_t *tempB = malloc(subMatrixSize * sizeof(float64x2_t));
+    
     pthread_t threads[7];
     ThreadData threadData[7];
     
-    matrix_addition_2x2(tempA, A, &A[2]);
-    matrix_addition_2x2(tempB, B, &B[2]);
-    threadData[0] = (ThreadData){M1, tempA, tempB};
-    pthread_create(&threads[0], NULL, compute_strassen_2x2, &threadData[0]);
+    matrix_addition(tempA, A, A + subMatrixSize * 2, newSize);
+    matrix_addition(tempB, B, B + subMatrixSize * 2, newSize);
+    threadData[0] = (ThreadData){M1, tempA, tempB, newSize};
+    pthread_create(&threads[0], NULL, compute_strassen, &threadData[0]);
     
-    matrix_addition_2x2(tempA, &A[2], &A[4]);
-    threadData[1] = (ThreadData){M2, tempA, B};
-    pthread_create(&threads[1], NULL, compute_strassen_2x2, &threadData[1]);
+    matrix_addition(tempA, A + subMatrixSize * 2, A + subMatrixSize * 4, newSize);
+    threadData[1] = (ThreadData){M2, tempA, B, newSize};
+    pthread_create(&threads[1], NULL, compute_strassen, &threadData[1]);
     
-    matrix_subtraction_2x2(tempB, &B[1], &B[3]);
-    threadData[2] = (ThreadData){M3, A, tempB};
-    pthread_create(&threads[2], NULL, compute_strassen_2x2, &threadData[2]);
+    matrix_subtraction(tempB, B + subMatrixSize, B + subMatrixSize * 3, newSize);
+    threadData[2] = (ThreadData){M3, A, tempB, newSize};
+    pthread_create(&threads[2], NULL, compute_strassen, &threadData[2]);
     
-    matrix_subtraction_2x2(tempB, &B[2], B);
-    threadData[3] = (ThreadData){M4, &A[3], tempB};
-    pthread_create(&threads[3], NULL, compute_strassen_2x2, &threadData[3]);
+    matrix_subtraction(tempB, B + subMatrixSize * 2, B, newSize);
+    threadData[3] = (ThreadData){M4, A + subMatrixSize * 3, tempB, newSize};
+    pthread_create(&threads[3], NULL, compute_strassen, &threadData[3]);
     
-    matrix_addition_2x2(tempA, A, &A[1]);
-    threadData[4] = (ThreadData){M5, tempA, &B[3]};
-    pthread_create(&threads[4], NULL, compute_strassen_2x2, &threadData[4]);
+    matrix_addition(tempA, A, A + subMatrixSize, newSize);
+    threadData[4] = (ThreadData){M5, tempA, B + subMatrixSize * 3, newSize};
+    pthread_create(&threads[4], NULL, compute_strassen, &threadData[4]);
     
-    matrix_subtraction_2x2(tempA, &A[2], A);
-    matrix_addition_2x2(tempB, B, &B[1]);
-    threadData[5] = (ThreadData){M6, tempA, tempB};
-    pthread_create(&threads[5], NULL, compute_strassen_2x2, &threadData[5]);
+    matrix_subtraction(tempA, A + subMatrixSize * 2, A, newSize);
+    matrix_addition(tempB, B, B + subMatrixSize, newSize);
+    threadData[5] = (ThreadData){M6, tempA, tempB, newSize};
+    pthread_create(&threads[5], NULL, compute_strassen, &threadData[5]);
     
-    matrix_subtraction_2x2(tempA, &A[1], &A[3]);
-    matrix_addition_2x2(tempB, &B[2], &B[3]);
-    threadData[6] = (ThreadData){M7, tempA, tempB};
-    pthread_create(&threads[6], NULL, compute_strassen_2x2, &threadData[6]);
+    matrix_subtraction(tempA, A + subMatrixSize, A + subMatrixSize * 3, newSize);
+    matrix_addition(tempB, B + subMatrixSize * 2, B + subMatrixSize * 3, newSize);
+    threadData[6] = (ThreadData){M7, tempA, tempB, newSize};
+    pthread_create(&threads[6], NULL, compute_strassen, &threadData[6]);
     
     for (int i = 0; i < 7; ++i) {
         pthread_join(threads[i], NULL);
     }
     
-    matrix_addition_2x2(C, M1, M4);
-    matrix_subtraction_2x2(C, C, M5);
-    matrix_addition_2x2(C, C, M7);
+    matrix_addition(C, M1, M4, newSize);
+    matrix_subtraction(C, C, M5, newSize);
+    matrix_addition(C, C, M7, newSize);
     
-    matrix_addition_2x2(&C[1], M3, M5);
-    matrix_addition_2x2(&C[2], M2, M4);
-    matrix_subtraction_2x2(&C[3], M1, M2);
-    matrix_addition_2x2(&C[3], &C[3], M3);
-    matrix_addition_2x2(&C[3], &C[3], M6);
-}
+    matrix_addition(C + subMatrixSize, M3, M5, newSize);
+    matrix_addition(C + subMatrixSize * 2, M2, M4, newSize);
+    
+    matrix_subtraction(C + subMatrixSize * 3, M1, M2, newSize);
+    matrix_addition(C + subMatrixSize * 3, C + subMatrixSize * 3, M3, newSize);
+    matrix_addition(C + subMatrixSize * 3, C + subMatrixSize * 3, M6, newSize);
 
-int main() {
-    float64x2_t A[8], B[8], C[8];
-    for (int i = 0; i < 8; i++) {
-        A[i] = vdupq_n_f64(i + 1);
-        B[i] = vdupq_n_f64(i + 2);
-    }
-    
-    strassen_2x2(C, A, B);
-    
-    for (int i = 0; i < 8; i++) {
-        double result[2];
-        vst1q_f64(result, C[i]);
-        printf("C[%d]: %lf %lf\n", i, result[0], result[1]);
-    }
-    return 0;
+    free(M1); free(M2); free(M3); free(M4);
+    free(M5); free(M6); free(M7);
+    free(tempA); free(tempB);
 }
