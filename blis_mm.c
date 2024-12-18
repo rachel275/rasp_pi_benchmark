9998c324361246cefc33ec5b76b9208f11910227
rachel275 <80170552+rachel275@users.noreply.github.com>
2024-12-18 11:43:05 +0000

    Update blis_mm.c to take double instead of float

--- blis_mm.c
+++ blis_mm.c
@@ -12,7 +12,6 @@
 #define C2 MAT_SIZE
 
 
-
 int main(){
 
 	//Initialize BLIS
@@ -20,9 +19,9 @@ int main(){
 
 	//dim_t m , k, n;
 
-	float* A = (float*)malloc(M * K * sizeof(float));
-	float* B = (float*)malloc(K * N * sizeof(float));
-	float* C = (float*)calloc(M * N,  sizeof(float));
+	double* A = (double*)malloc(M * K * sizeof(double));
+	double* B = (double*)malloc(K * N * sizeof(double));
+	double* C = (double*)calloc(M * N,  sizeof(double));
 	
 //	printf("reached here");
 
@@ -38,21 +37,21 @@ int main(){
     	// Input elements of A
     	//printf("Enter elements of matrix A (%lu x %lu):\n", m, k);
     	for (int i = 0; i < M * K; i++) {
-        	A[i] = rand();
+        	A[i] = (double)rand() / RAND_MAX;
     	}
 
     	// Input elements of B
     	//printf("Enter elements of matrix B (%lu x %lu):\n", k, n);
     	for (int i = 0; i < K * N; i++) {
-        	B[i] = rand();
+        	B[i] = (double)rand() / RAND_MAX;
     	}
 	
-	float alpha = 1.0f, beta = 0.0f;
+	double alpha = 1.0, beta = 0.0;
 
 	obj_t A_blis, B_blis, C_blis;
-	bli_obj_create_with_attached_buffer(BLIS_FLOAT, M, K, A, 1, K, &A_blis);
-	bli_obj_create_with_attached_buffer(BLIS_FLOAT, K, N, B, 1, N, &B_blis);
-	bli_obj_create_with_attached_buffer(BLIS_FLOAT, M, N, C, 1, N, &C_blis);
+	bli_obj_create_with_attached_buffer(BLIS_DOUBLE, M, K, A, 1, K, &A_blis);
+	bli_obj_create_with_attached_buffer(BLIS_DOUBLE, K, N, B, 1, N, &B_blis);
+	bli_obj_create_with_attached_buffer(BLIS_DOUBLE, M, N, C, 1, N, &C_blis);
 	
 //	printf("now here");
 
@@ -72,4 +71,3 @@ int main(){
 
 	return 0;
 }
-
